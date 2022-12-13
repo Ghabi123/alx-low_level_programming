97cfde4c175dd6c87b5c1b506a84cb513e1f022e
Your Name <you@example.com>
2022-12-13 06:23:22 -0800

    new task new skill

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,4 +2,10 @@
 /**
  * main-Entry point
  * Descrition:the program's description
- * Return 0: Always success
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
