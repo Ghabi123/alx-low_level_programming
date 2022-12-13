70b42c7e7c114bec75ccd176f370c1a4094fffdc
Your Name <you@example.com>
2022-12-13 05:20:04 -0800

    restarting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/*printing something*/
+/**
+ * main-Entry point
+ * Description: 'the program's description'
+ * Return: Always 0(success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
