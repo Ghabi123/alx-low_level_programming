029332e5e31d17b6872bacc9de6746c904112390
Your Name <you@example.com>
2022-12-16 06:01:52 -0800

    new easy task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - Returns the last digit of a given number
+ * @n: the given number
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
