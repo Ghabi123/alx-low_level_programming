451fc576cf50580423f8b0e003d0d17cf4d1d1c4
Your Name <you@example.com>
2022-12-16 07:01:20 -0800

    oh aint very easy task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * times_table - shows the multiplication table
+ * Return: Always 0(success)
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 10; b++)
+		{
+			if (a * b < 10)
+			{
+				_putchar(a * b + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(a * b / 10 + '0');
+				_putchar(a * b % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
