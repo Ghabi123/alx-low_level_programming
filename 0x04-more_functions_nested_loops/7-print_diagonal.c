a9b25ec38624c337c7493d3d6ffef2ccf98e6d25
Your Name <you@example.com>
2022-12-16 16:41:09 -0800

    omg pls

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_diagonal - prints a diagonale using \
- * @n: number of times the program prints \
- * Return: Always 0(success)
+ * print_diagonal - prints a diagonale using '\'
+ * @n: how many times the program prints '\'
+ * Return: Always 0(Done)
  */
 void print_diagonal(int n)
 {
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < (i-1); j++)
+			for (j = 0; j < (i - 1); j++)
 			{
 				_putchar(' ');
 			}
