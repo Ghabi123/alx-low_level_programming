63013a1d617938160868c81d637a7301330624c9
Your Name <you@example.com>
2022-12-16 16:10:23 -0800

    what now

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,10 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	for (i = 1; i <= n; i++)
 	{
-		_putchar('\n');
+		_putchar(95);
 	}
-	else
-	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
-	}
+
+	_putchar('\n');
 }
