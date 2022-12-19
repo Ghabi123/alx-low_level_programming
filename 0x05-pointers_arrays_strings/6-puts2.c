a15b204ee4a4d79e2e60f1ea06772e43311cfdb0
Your Name <you@example.com>
2022-12-19 14:47:46 -0800

    kk

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,34 +1,19 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every single caracter of a string
- * @str: an input string
- * Return: a string (always done)
+ * puts2 - prints every other caracter
+ * @str: An input string
+ * Return: always 1 Done
  */
-
 void puts2(char *str)
 {
-
 	int len = 0, i = 0;
 
-
 	while (str[len] != '\0')
-
 		len++;
-
 	len -= 1;
 
 	for (; i <= len; i += 2)
 		_putchar(str[i]);
-
 	_putchar('\n');
-
 }
-
-Footer
-
-© 2022 GitHub, Inc.
-
-Footer navigation
-
-Term
