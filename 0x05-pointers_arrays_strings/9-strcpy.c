ce8e727960ff94e63deaa7c20fed61b85e36c7f8
Your Name <you@example.com>
2022-12-19 15:34:24 -0800

    now ?

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,14 @@
-#include "main.h"
-
 /**
  * *_strcpy - a function that copies a string pointed by src to paste it
  * @dest: A pointer to destination of string
  * @src: A pointer to source string to copy from
- * Return: pointer to dest
+ * Return: Always true
  */
 char *_strcpy(char *dest, char *src)
 {
 	char *aux = dest;
 
 	while (*src)
-		*dest++ = *src++ + '\0';
+		*dest++ = *src++;
 	return (aux);
 }
