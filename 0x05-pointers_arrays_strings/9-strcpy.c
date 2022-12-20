34f1fcf73333940e7c43b3ac626560c7271b7123
Your Name <you@example.com>
2022-12-20 14:03:50 -0800

    kkk

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i = 0, len = 0;
 
-	while (src[i])
+	while (src[len] != '\0')
+		len++;
+
+	while (i <= len)
 	{
 		dest[i] = src[i];
 		i++;
