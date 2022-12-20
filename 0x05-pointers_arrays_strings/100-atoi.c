612b57449b8d99116ac6119162e06f12143194d9
Your Name <you@example.com>
2022-12-20 14:40:47 -0800

    kkkk

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,9 +7,9 @@
  */
 int _atoi(char *s)
 {
-		        int sign = 1;
-			unsigned int total = 0;
-			char null_flag = 0;
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
 
 			while (*s)
 			{
