465a6c9d34b7b6474298b533d75c04e6c20cbd90
Your Name <you@example.com>
2022-12-21 09:26:15 -0800

    kkkkk

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-void print_integer(int);
 /**
  * print_number - a function that prints an integer
  * @n: an input integer
@@ -9,29 +7,25 @@ void print_integer(int);
 void print_number(int n)
 {
 	if (n == 0)
+
 		_putchar('0');
 
-	else if (n < 0)
+	else
+
+	if (n < 0)
 	{
 		_putchar('-');
-		print_integer(n * -1);
+		while (n != 0)
+		{
+			_putchar(n % 10 + '0');
+			n = n / 10;
+		}
 	}
 	else
-		print_integer(n);
-}
-
-/**
- * print_integer - a function that prints an integer.
- * @m: an input unsigned integer
- * Return: Nothing
- */
-void print_integer(int m)
-{
-	int i = 1000000000;
-
-	for (; i >= 1; i /= 10)
-		if ((m / i) != 0)
+		while (n != 0)
 		{
-			_putchar((m / i) % 10 + '0');
+			_putchar(n % 10 + '0');
+			n = n / 10;
 		}
 }
+
