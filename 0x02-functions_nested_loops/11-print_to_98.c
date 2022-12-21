63cee7494a2f20c1205691a2acdcefb9c94900d2
Your Name <you@example.com>
2022-12-21 10:28:38 -0800

    kkk

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_to_98 - print from n to 98
+ * @n: the integer to start printing from
+ * Return: always true
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
