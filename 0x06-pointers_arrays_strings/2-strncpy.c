93b4589180a55ec43bd848eaf1936de41b11fef0
Your Name <you@example.com>
2022-12-22 08:51:15 -0800

    kkkkk

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 
 /**
  * _strncpy - a function that copys the strings.
- * @dest: an input string
+ * @dest: an input string.
  * @src: an input string
  * @n: an input integer
  * Return: A pointer to the resulting string
@@ -17,10 +17,12 @@ char *_strncpy(char *dest, char *src, int n)
 		srclen++;
 		src++;
 	}
+
 	srclen++;
 
 	if (n > srclen)
 		n = srclen;
+
 	src = start;
 
 	for (; i < n; i++)
