963f4e3eda23133a86fdbba1affb5ba17ec64f85
Your Name <you@example.com>
2022-12-22 04:09:41 -0800

    kk

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,8 +8,8 @@
 * @day: day of month
 * @year: year
 * Return: void
-*
 */
+
 void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
@@ -18,6 +18,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
